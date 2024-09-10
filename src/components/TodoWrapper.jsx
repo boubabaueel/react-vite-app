aa45e380f468118d6d9d010076d62fa15c1d052a
Boubacar Diallo <90484140+boubabaueel@users.noreply.github.com>
2024-09-10 14:41:39 -0400

    added todo form button and made began to make it active in todo wrapper. also impotred uuidv4

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TodoForm from './TodoForm'
 
+import uuidv4 from React
 const TodoWrapper = () => {
+  const [todos, setTodos] =useState([])
+  const addTodo = todo =>{
+    setTodos([...todos, {id: uuidv4(), task: todo, 
+      completed: false, isEditing: false
+    }])
+  }
   return (
     <div className='TodoWrapper'>
-      <TodoForm/>
+      <TodoForm addTodo= {addTodo}/>
     </div>
   )
 }
